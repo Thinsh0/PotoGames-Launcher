51e60e8b004090be9dc46f03be9a4db5a31eaf04
Thinsho <86961214+Thinsh0@users.noreply.github.com>
2025-04-19 00:13:15 +0200

    Update

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -2,7 +2,7 @@ const { DistributionAPI } = require('helios-core/common')
 
 const ConfigManager = require('./configmanager')
 
-exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/Thinsh0/ServerPotoGames/main/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/Thinsh0/ServerPotoGames/refs/heads/master/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
