765ff18680f844eaa7862023987468e1660907f3
Thinsho <86961214+Thinsh0@users.noreply.github.com>
2025-07-12 01:31:49 +0200

    Update Discord Link

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -2,7 +2,8 @@ const { DistributionAPI } = require('helios-core/common')
 
 const ConfigManager = require('./configmanager')
 
-exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/Thinsh0/PotoCine/main/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/Thinsh0/PotoDistribution/master/distribution.json'
+
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
